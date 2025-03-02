53fd462ca7e695acc35b85dcb798c1e93313ee31
Aashish Manchanda <aashishmanchanda777@gmail.com>
2025-03-03 02:20:22 +0530

    Configure Cloudflare Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://planet-nano.pages.dev",
   integrations: [mdx(), sitemap(), tailwind()],
   devToolbar: {
     enabled: false
